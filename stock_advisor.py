021910330cdb1c8ebadd8761c9484e5ec8efb786
LeonardoMdSACode <leonar_ansari_8@hotmail.com>
2023-04-25 20:06:31 +0100

    Tensorflow correct code for RMSE- train/test

--- stock_advisor.py
+++ stock_advisor.py
@@ -9,9 +9,9 @@ import yfinance as yf
 import mplfinance as mpf
 import tensorflow as tf
 
-
-df = yf.download("GBPUSD=X", start="1990-01-01")
-# df.to_csv('GBPUSD.csv')
+stock = 'GBPUSD=X'
+df = yf.download(stock)
+# df.to_csv('stock_data.csv')
 
 print(df)
 print(df.shape)
@@ -33,51 +33,36 @@ print(df.shape)
 print(df.describe())
 print(df.isnull().sum())
 
-scaler = MinMaxScaler(feature_range=(-1, 1))
-df['Close'] = scaler.fit_transform(df['Close'].values.reshape(-1, 1))
+# Split the data into training and testing sets
+train_size = int(len(df) * 0.8)
+train_data = df.iloc[:train_size]
+test_data = df.iloc[train_size:]
 
-# function to create train, test data given stock data and sequence length
+# Split the data into training and test sets
+# train_data = df[:'2022']
+# test_data = df['2022':]
 
+# Normalize the data
+scaler = MinMaxScaler()
+train_data = scaler.fit_transform(train_data)
+test_data = scaler.transform(test_data)
 
-def load_data(stock, look_back):
-    data_raw = stock.values  # convert to numpy array
-    data = []
+# Define the function to create time series data
+def create_dataset(data, window_size):
+    X, y = [], []
+    for i in range(len(data) - window_size):
+        X.append(data[i:i+window_size])
+        y.append(data[i+window_size])
+    return np.array(X), np.array(y)
 
-    # create all possible sequences of length look_back
-    for index in range(len(data_raw) - look_back):
-        data.append(data_raw[index: index + look_back])
+window_size = 30
+X_train, y_train = create_dataset(train_data, window_size)
+X_test, y_test = create_dataset(test_data, window_size)
 
-    data = np.array(data)
-    test_set_size = int(np.round(0.15*data.shape[0]))
-    train_set_size = data.shape[0] - (test_set_size)
-
-    x_train = data[:train_set_size, :-1, :]
-    y_train = data[:train_set_size, -1, :]
-
-    x_test = data[train_set_size:, :-1]
-    y_test = data[train_set_size:, -1, :]
-
-    return [x_train, y_train, x_test, y_test]
-
-
-look_back = 75
-x_train, y_train, x_test, y_test = load_data(df, look_back)
-print('x_train.shape = ', x_train.shape)
-print('y_train.shape = ', y_train.shape)
-print('x_test.shape = ', x_test.shape)
-print('y_test.shape = ', y_test.shape)
-
-# make training and test sets in tensorflow
-x_train = tf.convert_to_tensor(x_train, dtype=tf.float32)
-x_test = tf.convert_to_tensor(x_test, dtype=tf.float32)
-y_train = tf.convert_to_tensor(y_train, dtype=tf.float32)
-y_test = tf.convert_to_tensor(y_test, dtype=tf.float32)
-
-y_train.shape, x_train.shape
 
 # Showing the graph where training and test data divide
-train_size = len(x_train)
-test_size = len(x_test)
+train_size = len(X_train)
+test_size = len(X_test)
 
 plt.figure(figsize=(10, 5))
 plt.title('GBP/USD Exchange Rate')
@@ -87,9 +72,13 @@ plt.xlabel('Time')
 plt.ylabel('Scaled Closing Price')
 plt.legend()
 
-plt.axvline(x_train.shape[0], color='k', linestyle='--')
+plt.axvline(X_train.shape[0], color='k', linestyle='--')
 
 # plt.show()
+
+# Reshape y_train and y_test to be 2D arrays
+y_train = y_train.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
 
 # LSTM RNN
 print("LSTM RNN Model")
@@ -102,8 +91,8 @@ class ResetStatesCallback(tf.keras.callbacks.Callback):
       self.model.reset_states()
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=32, return_sequences=True), input_shape=[None, 1]),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=32, return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=32, return_sequences=True), input_shape=[window_size, 1]),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=32, return_sequences=False)),
     tf.keras.layers.Dense(units=1)
 ])
 
@@ -116,31 +105,30 @@ reset_states = ResetStatesCallback()
 model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
    "my_checkpoint.h5", save_best_only=True)
 early_stopping = tf.keras.callbacks.EarlyStopping(patience=50)
-model.fit(x_train, y_train, epochs=100,
-          validation_data=(x_test, y_test),
+history = model.fit(X_train, y_train, epochs=100,
+          validation_data=(X_test, y_test),
           callbacks=[early_stopping, model_checkpoint, reset_states])
 
-y_train_pred = model(x_train)
-y_test_pred = model(x_test)
-
-y_train = scaler.inverse_transform(y_train)
-y_test = scaler.inverse_transform(y_test)
-y_train_pred = scaler.inverse_transform(y_train_pred.numpy().reshape(-1, 1))
-y_test_pred = scaler.inverse_transform(y_test_pred.numpy().reshape(-1, 1))
-
-trainScore = math.sqrt(mean_squared_error(y_train[:, 0], y_train_pred[:, 0]))
-print('Train Score: %.5f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(y_test[:, 0], y_test_pred[:, 0]))
-print('Test Score: %.5f RMSE' % (testScore))
-
 model = tf.keras.models.load_model("my_checkpoint.h5")
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+# Make predictions on the train set
+y_train_pred = model.predict(X_train)
+
+# Calculate the RMSE
+train_rmse = np.sqrt(mean_squared_error(y_train, y_train_pred))
+print('Train Score: %.5f RMSE' % train_rmse)
+test_rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print('Test Score: %.5f RMSE' % test_rmse)
+
 
 figure, axes = plt.subplots(figsize=(15, 6))
 axes.xaxis_date()
 
 axes.plot(df[len(df)-len(y_test):].index, y_test,
           color='red', label='Real GBP/USD Stock Price')
-axes.plot(df[len(df)-len(y_test):].index, y_test_pred,
+axes.plot(df[len(df)-len(y_test):].index, y_pred,
           color='blue', label='Predicted GBP/USD Stock Price')
 
 plt.title('GBP/USD Stock Price Prediction')
